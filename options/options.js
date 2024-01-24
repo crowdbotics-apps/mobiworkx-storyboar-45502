b113f6721495428f650d5e3a8640b6f887ae6e7e
Crowdbotics <team@crowdbotics.com>
2024-01-24 11:06:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mobiworkx-storyboar-45502",
+  url: "https://mobiworkx-storyboar-45502.botics.co",
+  api: "https://mobiworkx-storyboar-45502.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
